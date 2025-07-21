f62ce5f52e003a220fe703004d82650f51fc846c
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-21 15:19:36 -0300

    Fix something

--- utils/UserResponse.cs
+++ utils/UserResponse.cs
@@ -8,8 +8,8 @@ namespace TodoListJwt.utils
     public class UserResponse
     {
         
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
     }
 }
